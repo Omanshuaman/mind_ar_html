d6a0c96247a6550afe42445cffb7763ecc03d00e
Omanshuamanarnxt <omanshuaman@email.com>
2023-11-29 16:48:40 +0530

    new build

--- 4_FLEduClass/4-5_ImageTrackingExample3/FLE_AudioTouch.js
+++ 4_FLEduClass/4-5_ImageTrackingExample3/FLE_AudioTouch.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded',() => {
         scene.add(light);
 
         //load 3d Model
-        const gltf = await loadGLTF('../3dModels/pizza_flip/Pizza flip animation.gltf');
+        const gltf = await loadGLTF('../3dModels/Pizza flip animation.gltf');
         gltf.scene.scale.set(0.5, 0.5, 0.5);
         gltf.scene.rotation.set(Math.PI/2,0,0);
         gltf.scene.position.set(0, 0, 0.1);
